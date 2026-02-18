345a92c2195917e76d4a968fe7044d689aefcbd6
Tarun <tarunappari23@gmail.com>
2026-02-18 15:39:35 +0530

    added animations to the navbar as well

--- components/common/navbar.jsx
+++ components/common/navbar.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/styles/common/navbar.module.scss";
+import BlurReveal from "../animations/BlurAnimation";
 
 const navLinks = [
   { href: "#about", label: "About" },
@@ -14,24 +15,28 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarInner}>
-        <Link href="/" className={styles.logo}>
-          <Image
-            src="/assets/logo/logo.png"
-            alt="Logo"
-            width={120}
-            height={40}
-            priority
-            className={styles.logoImage}
-          />
-        </Link>
+        <BlurReveal rootMargin="0px">
+          <Link href="/" className={styles.logo}>
+            <Image
+              src="/assets/logo/logo.png"
+              alt="Logo"
+              width={120}
+              height={40}
+              priority
+              className={styles.logoImage}
+            />
+          </Link>
+        </BlurReveal>
 
         <ul className={styles.navLinks}>
-          {navLinks.map(({ href, label }) => (
-            <li key={href}>
-              <Link href={href} className={styles.navLink}>
-                {label}
-              </Link>
-            </li>
+          {navLinks.map(({ href, label }, i) => (
+            <BlurReveal key={href} rootMargin="0px" delay={(i *= 300)}>
+              <li>
+                <Link href={href} className={styles.navLink}>
+                  {label}
+                </Link>
+              </li>
+            </BlurReveal>
           ))}
         </ul>
       </div>
